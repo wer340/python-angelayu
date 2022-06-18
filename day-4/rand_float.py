0fc1e460db92b591323fa8408a4fdea1db2a840a
hamed.sh <hsee1372@gmail.com>
2022-06-18 15:59:22 +0430

    day-4  rand and module

---
+++ day-4/rand_float.py
@@ -0,0 +1,7 @@
+import random
+import my_module
+
+number_score=random.randint(100, 300)
+float_score=random.random()
+
+print(f"your score is {number_score} and your float score is {float_score} , pi = {my_module.pi}")
