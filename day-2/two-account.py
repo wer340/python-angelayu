08141c5de2c3ae4fb59f6f48ca52aa3ec3f9a539
wer340 <hsee1372@gmail.com>
2022-06-18 09:32:26 +0430

    two account patch bug

--- day-2/two-account.py
+++ day-2/two-account.py
@@ -1,4 +1,32 @@
 print("calceulator daily sell progit and cost for two person!!!")
+print('''
+ .                                                            ,
+  |`.                                                        ,'|
+  \_`-._                                                  _,-'_/
+ ./ \-._`-._                                          _,-'_,-/ \,
+ \._/._ `._>`-.__                                __,-'<_,' _,\_,/
+ /_ \_.`-._\_-._ `--__                      __--' _,-_/_,-',_/ _\
+  /._`\_./ _`_./`.-._ `-.                ,-' _,-,'\,_'_ \,_/'_,\
+   \._`/ _/ _`_./  _/`\_ `.            ,' _/'\_  \,_'_ \_ \'_,/
+    /._`/ _/ _`_./` _/ `\_ `\_      _/' _/' \_ '\,_'_ \_ \'_,\
+     \._`/ _/ _`_./` __/  >.  `-.,-'  ,<  \__ '\,_'_ \_ \'_,/
+       /_`/._/._`_./` __.`.-\_.-`'-._/-,',__ '\,_'_,\_,\'_\
+             `\._`_./`\._/_/'    _   `\_\_,/'\,_'_,/'
+                                /_\
+                                \./
+                              __/(__
+                             / `--' \
+                            / _    _ \
+                            \'|    |`/
+                             \|____|/
+                             \/ -] \/
+                             /   |  \
+                             |   |  |
+                             |   |   \
+                            /    |    \
+                            \__,-`\___/
+                            (_)     |__)             mic
+''')
 message="megdar daryafti x ra vard konid  ? "
 daryaftix=float(input(message))
 message="megdar daryafti y ra vard konid  ? "
@@ -13,19 +41,20 @@ dastmozd=float(input(message))
 hazineh=hazinehx+hazinehy
 daryafti=daryaftix+daryaftiy
 megdar_daryafti_kol=hazineh+dastmozd-daryafti
-megdar_daryafti_kol_x=hazinehx-daryaftix
-megdar_daryafti_kol_y=hazinehy-daryaftiy
+megdar_daryafti_kol_x=daryaftix-hazinehx
+megdar_daryafti_kol_y=daryaftiy-hazinehy
 message=f"megdar kol daryafti shoma as moshtari  brabar ast ba = {megdar_daryafti_kol} "
 print(message)
-if(megdar_daryafti_kol_x>megdar_daryafti_kol_y)
+print(f"x= {megdar_daryafti_kol_x} and y= {megdar_daryafti_kol_y}")
+if megdar_daryafti_kol_x>megdar_daryafti_kol_y:
    a= (megdar_daryafti_kol_x+megdar_daryafti_kol_y)/2
    bazpardakht_be_y=megdar_daryafti_kol_x-a
    message=f"shakhseh x bayed {bazpardakht_be_y} be shakseh y bedahad"
    print(message)
-    else{
-            a= (megdar_daryafti_kol_x+megdar_daryafti_kol_y)/2
-           bazpardakht_be_x=megdar_daryafti_kol_y-a
-            message=f"shakhseh y bayed {bazpardakht_be_x} be shakseh x bedahad"
-              print(message)
+else:
+   a= (megdar_daryafti_kol_x+megdar_daryafti_kol_y)/2
+   bazpardakht_be_x=megdar_daryafti_kol_y-a
+   message=f"shakhseh y bayed {bazpardakht_be_x} be shakseh x bedahad"
+   print(message)
         
 
