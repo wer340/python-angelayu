44f72591b5eaa2d965d8bba5857354646a23581b
wer340 <hsee1372@gmail.com>
2022-06-19 09:19:05 +0430

    scissors-paper-rock game complete

--- day-4/scissors_paper_rock.py
+++ day-4/scissors_paper_rock.py
@@ -1,3 +1,4 @@
+from ctypes import windll
 import random
 scissors=''''
      ______
@@ -27,7 +28,19 @@ paper='''
 number=int(input("Scissors,Rock,Paper game ? type 0 for rock ,type 1 for paper,type 2 for scissors ? "))
 
 my=[rock,paper,scissors]
-print(my[number])
+my_choice=my[number]
+print(my_choice)
 machine=random.randint(0,2)
-print(f"computer choose : \n {my[machine]}")
+machine_choice=my[machine]
+print(f"computer choose : \n {machine_choice}")
 
+# state=[[rock,scissors],[paper,rock],[scissors,paper],[paper,scissors],[rock,paper],[scissors,rock],[scissors,scissors],[paper,paper],[rock,rock]]
+win=[[rock,scissors],[paper,rock],[scissors,paper]]
+lose=[[paper,scissors],[rock,paper],[scissors,rock]]
+equal=[[scissors,scissors],[paper,paper],[rock,rock]]
+if   my_choice==machine_choice:
+     print("you equal please try again")
+elif win.count([my_choice,machine_choice])==1:
+     print("you win")
+else:
+     print("you lose")
