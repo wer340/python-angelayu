bff12f9173ef226a32d68e7364be7a297294337e
hossein <37295116+wer340@users.noreply.github.com>
2022-06-24 17:56:57 +0430

    Create readme.mk

---
+++ day-8/readme.mk
@@ -0,0 +1,5 @@
+#  input function
+
+import notice   thats  paraameter an argument    dont confuse two thing 😒
+
+<img src="https://raw.githubusercontent.com/wer340/python-angelayu/main/day-8/aparameter_aargument.png">
