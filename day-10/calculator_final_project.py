e6f7344348328357512115da79ca64ee7e51c09a
wer340 <hsee1372@gmail.com>
2022-06-27 08:05:34 +0430

    finish while loop

--- day-10/calculator_final_project.py
+++ day-10/calculator_final_project.py
@@ -2,6 +2,9 @@
 
 
 # add
+from typing import final
+
+
 def add(a,b):
     return a+b
 # subtract
@@ -13,8 +16,7 @@ def multiply(a,b):
 
 #divide
 def divide(a,b):
-    result=round(a/b,6)
-    return result
+        return a/b
 
 operator={
     "+":add ,
@@ -22,7 +24,7 @@ operator={
     "*":multiply ,
     "/":divide 
 }
-
+    
 num1=int(input("whats first number ? : "))
 num2=int(input("whats second number ? : "))
 for key in operator:
@@ -32,11 +34,23 @@ def function_call(operator_in):
     """give a sign opration give a function calc"""
     for key in operator:
         if operator_in==key:
-            function=operator[key]
-            return function 
+            function_symbol=operator[key]
+            return function_symbol 
 
-def calculator(num1,num2):
+def calculator(num1,num2,operator_in):
     func=function_call(operator_in)
-    return func(num1,num2)
-final=calculator(num1,num2)
-print(f"{num1} {operator_in} {num2} = {final}")
+    final=func(num1,num2)
+    print(f"{num1} {operator_in} {num2} = {final}")
+    calc_flag=True
+    while calc_flag:
+        decion=input("are you continu to calc ? y or no")
+        if decion=="n":
+            return "calc finish"
+        elif decion!="y":
+            print ("invalid number")
+        operator_in=input("whats operator ? : ")
+        num2=int(input("whats other number ? : "))
+        
+        print(f"{final} {operator_in} {num2} = {func(final,num2)}")
+        final=func(final,num2)
+calculator(num1,num2,operator_in)
