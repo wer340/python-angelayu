7d1ff4cbb79c955764c66275f2c91d0627f65dfd
wer340 <hsee1372@gmail.com>
2022-06-27 08:46:00 +0430

    make while loop term of angela

--- day-10/calculator_final_project.py
+++ day-10/calculator_final_project.py
@@ -2,8 +2,6 @@
 
 
 # add
-from typing import final
-
 
 def add(a,b):
     return a+b
@@ -25,11 +23,7 @@ operator={
     "/":divide 
 }
     
-num1=int(input("whats first number ? : "))
-num2=int(input("whats second number ? : "))
-for key in operator:
-    print(key)
-operator_in=input("whats operator ? : ")
+
 def function_call(operator_in):
     """give a sign opration give a function calc"""
     for key in operator:
@@ -37,20 +31,26 @@ def function_call(operator_in):
             function_symbol=operator[key]
             return function_symbol 
 
-def calculator(num1,num2,operator_in):
-    func=function_call(operator_in)
-    final=func(num1,num2)
-    print(f"{num1} {operator_in} {num2} = {final}")
+def calculator():
+
+    num1=int(input("whats first number ? : "))
     calc_flag=True
     while calc_flag:
-        decion=input("are you continu to calc ? y or no")
+        num2=int(input("whats second number ? : "))#input int2
+        for key in operator:
+            print(key)   #print symbol
+        operator_in=input("whats operator ? : ") #input symbol
+        func=function_call(operator_in)
+        final=func(num1,num2)
+        print(f"{num1} {operator_in} {num2} = {final}")
+        decion=input("are you continu to calc ? y or n ")
         if decion=="n":
-            return "calc finish"
+            calc_flag=False
+            calculator()
         elif decion!="y":
             print ("invalid number")
-        operator_in=input("whats operator ? : ")
-        num2=int(input("whats other number ? : "))
+        else:
+            num1=final
         
-        print(f"{final} {operator_in} {num2} = {func(final,num2)}")
-        final=func(final,num2)
-calculator(num1,num2,operator_in)
+        
+calculator()
