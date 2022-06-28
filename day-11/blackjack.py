b6a344a16500be81efb33ec90d5bed303fe6c3ec
wer340 <hsee1372@gmail.com>
2022-06-28 12:01:46 +0430

    attemp second

--- day-11/blackjack.py
+++ day-11/blackjack.py
@@ -23,66 +23,45 @@ def blaackjack_loop(user,computer):
     sum_user=sum(user)
     sum_computer=sum(computer)
     
-    if sum_user>20:
-        if len(user)<3:
-            if sum_user==21:
-                return False
-        elif sum_computer==21:
-             return True
-        if user.count(11)==1:
-            index=user.index(11)
-            user[index]=1
-            sum_new_user=sum(user)
-            if sum_new_user>21:
-                return False
-            else :
-                return "continue"
-        else:
-            return False
+    if sum_user>21:
+        return False
     elif sum_computer>21 :
-        if computer.count(11)==1:
-            index=computer.index(11)
-            computer[index]=1
-            computer_new_user=sum(computer)
-            if computer_new_user>21:
-                return True
-            else :
-                return "continue"
-        else:
-            return True
+        return True
     else:
         return None
 
 
+answer=blaackjack_loop(user,computer)
 
 
-
-if  blaackjack_loop(user,computer)==False:
+if  answer==False:
     print("you lose")
-elif  blaackjack_loop(user,computer)==True :
+elif  answer==True :
     print("you win")
 else:
-    while blaackjack_loop(user,computer)==None:
+    while answer==None:
         ask_user=input("whether you pick card or? y or n :" )
         if ask_user=="y":
                 input("type any keyboard ")
                 pickup(user)
                 print(f"\n user card = {user} \n")
                 print(f"\n computer card = {computer} \n")
-                blaackjack_loop(user,computer)
-                if blaackjack_loop(user,computer)==True:
-                            print("you win")
-                elif blaackjack_loop(user,computer)==False:
-                            print("you lose")
+                answer=blaackjack_loop(user,computer)
+                if sum(user)>sum(computer) and answer==None:
+                            break
         else:
-                while blaackjack_loop(user,computer)=="continue":
+                while answer==None:
                         pickup(computer)
                         print(f"\n computer card = {computer} \n")
-                        blaackjack_loop(user,computer)
-                        if blaackjack_loop(user,computer)==True:
-                            print("you win")
-                        elif blaackjack_loop(user,computer)==False:
-                            print("you lose")
+                        answer=blaackjack_loop(user,computer)
+                        if sum(user)<sum(computer) and answer==None :
+                            answer=False
+                            break 
+
+if  answer==False or sum(user)<sum(computer) :
+    print("you lose")
+elif  answer==True or sum(user)>sum(computer) :
+    print("you win")                      
 
 # user_deciton=True         
 # while user_deciton:    
