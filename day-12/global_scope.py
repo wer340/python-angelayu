008f75dc58d023c371dbb339327977b042c8c435
hamed.sh <hsee1372@gmail.com>
2022-06-29 15:42:50 +0430

    refrence to global var replce define var in scope

--- day-12/global_scope.py
+++ day-12/global_scope.py
@@ -3,7 +3,7 @@
 enemie=2
 
 def my_function():
-    enemie=0
+    global enemie
     enemie +=5  #refrence before assignment mean user in scope not define var  and  process on var that not define
     
     print(f"your enemis is local scope : {enemie}")
