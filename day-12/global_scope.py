1625c076bfb5659df7082ac11f8908e5637f6720
hamed.sh <hsee1372@gmail.com>
2022-06-29 15:41:39 +0430

    resolve assignment before define

--- day-12/global_scope.py
+++ day-12/global_scope.py
@@ -3,7 +3,9 @@
 enemie=2
 
 def my_function():
-    enemie+=1
+    enemie=0
+    enemie +=5  #refrence before assignment mean user in scope not define var  and  process on var that not define
+    
     print(f"your enemis is local scope : {enemie}")
 
 my_function()
