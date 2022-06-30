59bca920c3241dabbf5d28e31ebfb516a8ab9b9b
wer340 <hsee1372@gmail.com>
2022-06-30 09:52:12 +0430

    issue play_computer

---
+++ day-13/play_computer.py
@@ -0,0 +1,7 @@
+year=int(input("whats your year of birth?  :"))
+if year>1980 and year <1994:
+    print("You aare a millenial.")
+elif year>1994:
+    print("You are a Gen Z")
+
+# if 1994  type in input??
