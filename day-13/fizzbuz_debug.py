ed340b6f04ee9b0bdbf653ea133cc8d62beef2b1
hamed.sh <hsee1372@gmail.com>
2022-06-30 15:54:20 +0430

    fix bug  if elif

--- day-13/fizzbuz_debug.py
+++ day-13/fizzbuz_debug.py
@@ -1,9 +1,12 @@
 for number in range(1,101):
-    if number%3==0 or number%5==0:
+    if number%3==0 and number%5==0:
         print("Fizz Buzz")
-    if number%3==0:
+    elif number%3==0:
         print("fizz")
-    if number%5==0:
+    elif number%5==0:
         print("buzz")
     else:
         print([number])
+
+# if   then if seq  for a uniq codition not used  if and elif and for common thing or means  one of them or both of them
+
