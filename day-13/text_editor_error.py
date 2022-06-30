8477e614a5f242075901c521f4d3077612bb9be9
wer340 <hsee1372@gmail.com>
2022-06-30 10:21:30 +0430

    resolve frustrating bug without sign

--- day-13/text_editor_error.py
+++ day-13/text_editor_error.py
@@ -1,4 +1,4 @@
 age=int(input("How old are You ? "))
 if age>18:
-    print("You can drive at age {age}") #frustrating no error no search google and  
-    #this error face string
+    print(f"You can drive at age {age}") #frustrating no error no search google and  
+    #this error face string  experience becaause of resolve this problem 
