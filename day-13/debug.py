6a0283423288a6cbd57dd54df39877431be6b916
wer340 <hsee1372@gmail.com>
2022-06-30 09:24:41 +0430

    bug code

---
+++ day-13/debug.py
@@ -0,0 +1,11 @@
+def my_function():
+    for i in range(1,20):
+        if i==20:
+            print("you got it")
+
+my_function()
+
+#what is the for-loop  doing?
+#1>20 counter and i==20 print 
+#when is the function meant to print "You got it"  answer i==20
+#what are you assumptions about i  till 1-19
