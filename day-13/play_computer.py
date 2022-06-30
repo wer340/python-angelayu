d6b90accc0e522c16d62dcc0e05bdcf257f23dae
wer340 <hsee1372@gmail.com>
2022-06-30 09:57:46 +0430

    fix bug by play computer method second

--- day-13/play_computer.py
+++ day-13/play_computer.py
@@ -1,5 +1,5 @@
 year=int(input("whats your year of birth?"))
-if year>1980 and year <=1994:
+if year>1980 and year <1994:
     print("You are a millenial.")
-elif year>1994:
+elif year>=1994:
     print("You are a Gen Z")
