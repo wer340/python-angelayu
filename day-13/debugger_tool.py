65d1622f3a1ac468a93c479b22667cccffbad82b
wer340 <hsee1372@gmail.com>
2022-06-30 11:46:19 +0430

    find problem with debugger

--- day-13/debugger_tool.py
+++ day-13/debugger_tool.py
@@ -2,7 +2,7 @@ def mutate(a_list):
     b_list=[]
     for item in a_list:
         new_item=item*2
-    b_list.append(new_item) #use debugger for this issue
+    b_list.append(new_item) #pythontutor.com   this line not there in for -loop
     print(b_list)
 
 mutate([1,2,3,5,8,13])
