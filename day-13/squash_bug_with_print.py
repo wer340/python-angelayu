524e38be82998f0112ce9b0c642bc5300123e344
wer340 <hsee1372@gmail.com>
2022-06-30 10:51:50 +0430

    fix bug with helping  print

--- day-13/squash_bug_with_print.py
+++ day-13/squash_bug_with_print.py
@@ -1,8 +1,8 @@
 pages=0
 word_per_page=0
 pages=int(input("Number of pages :  "))
-word_per_page==int(input("Number of words per pages :  "))#this is bug 
+word_per_page=int(input("Number of words per pages :  "))
 total_words=pages*word_per_page
 print(f"pages={pages}")
-print(f"word_per_pages={word_per_page}") #you can see this data zero  manage and narrow down problem 
+print(f"word_per_pages={word_per_page}")
 print(total_words)
