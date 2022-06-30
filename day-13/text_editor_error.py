75a58be6cfffd181c56b7f77ad5f9c2f2d900942
wer340 <hsee1372@gmail.com>
2022-06-30 10:13:12 +0430

    frustrating error without sign of err

--- day-13/text_editor_error.py
+++ day-13/text_editor_error.py
@@ -1,3 +1,4 @@
 age=int(input("How old are You ? "))
 if age>18:
-    print("You can drive at age {age}")
+    print("You can drive at age {age}") #frustrating no error no search google and  
+    #this error face string
