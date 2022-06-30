f3cb0cd9ce6c4fb2d524bcadb7421c7882280859
wer340 <hsee1372@gmail.com>
2022-06-30 09:55:36 +0430

    imagin you are a computer

--- day-13/play_computer.py
+++ day-13/play_computer.py
@@ -1,7 +1,7 @@
-year=int(input("whats your year of birth?  :"))
-if year>1980 and year <1994:
-    print("You aare a millenial.")
-elif year>1994:
-    print("You are a Gen Z")
+year=1994  #assumption yoau are computer 
+# if ✅1994>1980 and ❌1994 <1994: this condition is false then next condition
+#     print("You aare a millenial.")
+# elif ❌1994>1994:   this condition false then next condition  that no exist
+#     print("You are a Gen Z")
 
 # if 1994  type in input??
