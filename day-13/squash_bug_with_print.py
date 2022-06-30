6ab0946cc0930bc41d1d6465880eae46be4b9223
wer340 <hsee1372@gmail.com>
2022-06-30 10:50:57 +0430

    find bug by print  with narrow down  block of code

--- day-13/squash_bug_with_print.py
+++ day-13/squash_bug_with_print.py
@@ -1,6 +1,8 @@
 pages=0
 word_per_page=0
 pages=int(input("Number of pages :  "))
-word_per_page==int(input("Number of words per pages :  "))
+word_per_page==int(input("Number of words per pages :  "))#this is bug 
 total_words=pages*word_per_page
+print(f"pages={pages}")
+print(f"word_per_pages={word_per_page}") #you can see this data zero  manage and narrow down problem 
 print(total_words)
