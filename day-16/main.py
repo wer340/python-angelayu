b60a3808a685cac52274c055c3cca7445d030711
wer340 <hsee1372@gmail.com>
2022-07-04 18:34:02 +0430

    angela solution oop caffe machin

--- day-16/main.py
+++ day-16/main.py
@@ -3,4 +3,13 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# name=input("give type caaffe")
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+coffee_maker.report()
+money_machine.report()
+menu = Menu()
+menu_item=MenuItem
+
+item = menu.get_items()
+name = input(f"give type caffe {item}  ? : ")
+
