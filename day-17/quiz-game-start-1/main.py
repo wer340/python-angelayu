85dbb2d365c236df0f0b9b122e69411421b60e2f
wer340 <hsee1372@gmail.com>
2022-07-05 17:52:53 +0430

    solve issue  list indices must be integers or slices, not str

--- day-17/quiz-game-start-1/main.py
+++ day-17/quiz-game-start-1/main.py
@@ -1,8 +1,9 @@
 from data import question_data
 from question_model import Question
 len=len(question_data)
-i=1
-date=["q_1","q_2","q_3","q_4","q_5","q_6","q_7","q_8","q_9","q_10","q_11","q_12"]
+question_bank=[]
 for item in question_data:
-    date[i]=Question(question_data[item]["text"], question_data[item]["answer"])
-    i+=1
+    question_bank.append(Question(item["text"],item["answer"]))
+
+print(question_bank[10].text)
+print(question_bank[10].answer)
