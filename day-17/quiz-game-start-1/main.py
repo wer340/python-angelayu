d860f65d7c7e91b7d5d74a9df8b634d3166d502e
wer340 <hsee1372@gmail.com>
2022-07-05 19:19:27 +0430

    compelete quiz by own way

--- day-17/quiz-game-start-1/main.py
+++ day-17/quiz-game-start-1/main.py
@@ -1,9 +1,18 @@
 from data import question_data
 from question_model import Question
-len=len(question_data)
-question_bank=[]
-for item in question_data:
-    question_bank.append(Question(item["text"],item["answer"]))
+from quiz_brain import QuizBrain
 
-print(question_bank[10].text)
-print(question_bank[10].answer)
+len = len(question_data)
+question_bank = []
+for item in question_data:
+    question_bank.append(Question(item["text"], item["answer"]))
+
+quiz = QuizBrain(question_bank)
+loss = False
+while not loss:
+    ask = quiz.next_qestion()
+    if not quiz.still_has_question(ask):
+       loss=True
+
+    print(f"your correct score :{quiz.question_list[quiz.question_number-1].answer}")
+    print(f"your  score :{quiz.question_number}")
--- day-17/quiz-game-start-1/quiz_brain.py
+++ day-17/quiz-game-start-1/quiz_brain.py
@@ -1,9 +1,18 @@
 class QuizBrain:
-    def __init__(self,q_list):
-        self.question_number=0
-        self.question_list=q_list
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
 
-    def next(self,):
-        for item in self.question_list:
-            self.question_number+=1
-            answer_q=input(f"{item.tetx} :True or False")
+    def next_qestion(self, ):
+        current_q = self.question_list[self.question_number]
+        self.question_number += 1
+        answer_q = input(
+            f"Q.{self.question_number} - {current_q.text} :True/False :? ")
+        return answer_q
+
+    def still_has_question(self, ask):
+        if self.question_list[self.question_number - 1].answer == ask:
+            print("You got it right!")
+            return True
+        else:
+            return False
