c9fb0964c2d207b18f7ac5036f3aad0a7e7fecac
wer340 <hsee1372@gmail.com>
2022-07-05 17:42:48 +0430

    issue name for object in loop still failure

--- day-17/quiz-game-start-1/main.py
+++ day-17/quiz-game-start-1/main.py
@@ -1,8 +1,8 @@
 from data import question_data
 from question_model import Question
 len=len(question_data)
-
+i=1
+date=["q_1","q_2","q_3","q_4","q_5","q_6","q_7","q_8","q_9","q_10","q_11","q_12"]
 for item in question_data:
-    first=Question(question_data["text"], question_data["answer"])
-
-
+    date[i]=Question(question_data[item]["text"], question_data[item]["answer"])
+    i+=1
