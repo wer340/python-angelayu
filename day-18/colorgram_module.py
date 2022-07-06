ce8804f2dfd65f928767eb06d3aa2ddf682443cb
wer340 <hsee1372@gmail.com>
2022-07-06 18:54:07 +0430

    colorgram fix

--- day-18/colorgram_module.py
+++ day-18/colorgram_module.py
@@ -1,7 +1,7 @@
 from PIL.Image import Image
-import colorgram_module
-colors=colorgram_module.extract("Image/colorgram.jpg",22)
-
+import colorgram
+colors=colorgram.extract("Image/colorgram.jpg",22)
+color_rgb=[]
 for color in colors:
     r=color.rgb.r    
     g=color.rgb.g
