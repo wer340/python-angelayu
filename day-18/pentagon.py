1a0f614fb0804504bd1ddc4ff586813141d06f9e
wer340 <hsee1372@gmail.com>
2022-07-06 14:33:01 +0430

    pentagon challenge solve

---
+++ day-18/pentagon.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+from random import choice
+
+color_pantagon = ["chartreuse", "cyan", "chocolate", "coral", "blue",
+                  "brown", "azure", "pink", "gold", "DogerBlue",
+                  "linen", "purple", "yellow", "salmon", "tomato","wheat","peru","green"]
+
+timmy = Turtle()
+screen = Screen()
+screen.bgcolor("black")
+timmy.penup()
+timmy.left(90)
+timmy.forward(300)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(180)
+timmy.pendown()
+for item in range(3, 10):
+    timmy.color(choice(color_pantagon))
+    for _ in range(item):
+        timmy.forward(200)
+        timmy.right(360 / item)
+
+screen.exitonclick()
---
+++ day-18/turtle_course.py
@@ -0,0 +1,15 @@
+from turtle import Turtle,Screen
+
+timmy=Turtle()
+
+screen=Screen()
+timmy.shape("turtle")
+timmy.color("yellowgreen")
+timmy.forward(290)
+timmy.left(90)
+timmy.forward(290)
+timmy.left(90)
+timmy.forward(290)
+timmy.left(90)
+timmy.forward(290)
+screen.exitonclick()
