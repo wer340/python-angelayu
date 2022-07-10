8c85a288ced67c70cfaabdd2be4cbd5a86843d85
wer340 <hsee1372@gmail.com>
2022-07-10 10:32:50 +0430

    create loop best practise

--- day-20/snake.py
+++ day-20/snake.py
@@ -1,28 +1,13 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 
-
-
-
-
-
-screen=Screen()
-screen.setup(width=600,height=600)
+screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-#three timmy  repeat a work  so use for
-timmy1=Turtle(shape="square")
-timmy1.color("white")
-timmy2=Turtle(shape="square")
-timmy2.color("orange")
-timmy2.goto(-20,0)
-timmy3=Turtle(shape="square")
-timmy3.color("yellow")
-timmy3.goto(-40,0)
-
-
-
-
-
-
-
+# three timmy  repeat a work  so use for
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
