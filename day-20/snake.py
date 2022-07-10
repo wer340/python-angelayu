a2398f19ac46cc4fb748509c059430669419af59
wer340 <hsee1372@gmail.com>
2022-07-10 10:29:35 +0430

    repeat work

--- day-20/snake.py
+++ day-20/snake.py
@@ -2,16 +2,20 @@ from turtle import Turtle,Screen
 
 
 
-timmy1=Turtle(shape="square")
-timmy2=Turtle(shape="square")
-timmy3=Turtle(shape="square")
+
+
+
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
+#three timmy  repeat a work  so use for
+timmy1=Turtle(shape="square")
 timmy1.color("white")
+timmy2=Turtle(shape="square")
 timmy2.color("orange")
-timmy3.color("yellow")
 timmy2.goto(-20,0)
+timmy3=Turtle(shape="square")
+timmy3.color("yellow")
 timmy3.goto(-40,0)
 
 
