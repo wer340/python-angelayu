e53f26cb12fc7c5425bdd88c4f8c507533b5288d
wer340 <hsee1372@gmail.com>
2022-07-10 10:20:45 +0430

    turtle body

---
+++ day-20/snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle,Screen
+
+
+
+timmy1=Turtle(shape="square")
+timmy2=Turtle(shape="square")
+timmy3=Turtle(shape="square")
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+timmy1.color("white")
+timmy2.color("orange")
+timmy3.color("yellow")
+timmy2.goto(-20,0)
+timmy3.goto(-40,0)
+
+
+
+
+
+
+
+
+screen.exitonclick()
