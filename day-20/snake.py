c9b6232a565729e9c984d445e4a06a7e0a2bc6d3
wer340 <hsee1372@gmail.com>
2022-07-10 11:45:45 +0430

    animation and gap issue

--- day-20/snake.py
+++ day-20/snake.py
@@ -5,9 +5,24 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 # three timmy  repeat a work  so use for
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
+segment=[]
 for position in starting_position:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
     new_segment.goto(position)
+    segment.append(new_segment)
+#turn on animation  section Animation control
+
+game_on=True
+while game_on:
+    for sag in segment:
+        sag.forward(20)
+        screen.delay(300)
+
+
+
+
+
+
 
 screen.exitonclick()
