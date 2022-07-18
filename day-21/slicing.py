8c15f90e66d5e4df2e39bb9c281baa59fccc97dd
wer340 <hsee1372@gmail.com>
2022-07-18 10:16:19 +0430

    slicing consept

---
+++ day-21/slicing.py
@@ -0,0 +1,8 @@
+actress=['Mila Kunis', 'Jennifer Lawrence', 'Olivia Wilde', 'Nina Dobrev',
+         'Evan Rachel Wood', 'Victoria Justice', 'Megan Fox', 'Blake Lively',
+         'Amber Heard', 'Camilla Belle', 'Dianna Agron', 'Emma Watson']
+
+print(len(actress))
+print(actress[::3])
+print(actress[::-3])
+print(actress[6:])
