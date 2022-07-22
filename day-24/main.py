c67e5277095f17773bd348c1480a0d9a5d3dd6a9
wer340 <hsee1372@gmail.com>
2022-07-22 21:39:47 +0430

    snake game high score complete

---
+++ day-24/main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from Snake import Snake
+from Food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+snake = Snake()
+food = Food()
+score_board = ScoreBoard()
+screen.setup(width=600, height=600)
+screen.title("snake Game")
+screen.tracer(0)  # set it zero for turn off
+# three timmy  repeat a work  so use for
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+# turn on animation  section Animation control
+
+game_on = True
+while game_on:
+    score_board
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        # distance method measure distance between own instance with other instance inside prantess
+        score_board.increase_score()
+        snake.extend()
+        food.refresh()
+        print("wonderful")  # detect collision with wall
+    if snake.head.xcor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        score_board.reset()
+        snake.reset()
+
+    # detect collision with tail  # if any seg collision with each seg (tail) trigger game over
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score_board.reset()
+            snake.reset()
+
+screen.exitonclick()
