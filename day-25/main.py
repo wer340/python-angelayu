0559b223de97c1d2b2105441ad2fa2578d631534
wer340 <hsee1372@gmail.com>
2022-07-24 12:15:49 +0430

    Serialization / IO / conversion

--- day-25/main.py
+++ day-25/main.py
@@ -1,5 +1,8 @@
 import pandas
 with open("./wheather_sheet.csv") as wheather:
     data=pandas.read_csv(wheather)
-
-    print(data["temp"])
+    fa="dd"
+    print(type(data) )
+    print(type(data["temp"]) )
+    print(data.to_dict())
+    print(data["temp"].to_list())
