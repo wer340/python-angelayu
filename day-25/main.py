2307a7dd21107d4dfaf29c476c5164342086363f
wer340 <hsee1372@gmail.com>
2022-07-25 08:42:44 +0430

    complete

--- day-25/main.py
+++ day-25/main.py
@@ -13,19 +13,19 @@ score=0
 game_on = True
 while game_on:
     answer = screen.textinput(title="guess the state ",
-                              prompt="whats another state ")
-    state = data["state"]
+                              prompt="whats another state  q=exit")
+    state = data["state"]   #should use data["state"].to_list
 
     for city in state:
-        if city == answer:
+        if city == answer: #shuld use if city in state:
             city_inf = data[data["state"] == answer]
             print(city_inf.x)
             texter.goto(int(city_inf["x"]),int(city_inf.y))
-            texter.write(answer)
+            texter.write(city_inf.state.item()) #answer ~ city_inf.state.item()
             score+=1
     if answer == "q":
         game_on = False
-    if score==3:
+    if score==50:
         texter.goto(-60,0)
         texter.color("green")
         texter.write("You win",font=["Arial",40,"normal"])
