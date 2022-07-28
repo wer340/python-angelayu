352f3282a7d0daddf5aa4651b519c4345c40d12c
wer340 <hsee1372@gmail.com>
2022-07-28 12:29:16 +0430

    grid place

--- day-27/grid_palce.py
+++ day-27/grid_palce.py
@@ -3,11 +3,10 @@ import tkinter
 windows = tkinter.Tk()
 windows.minsize(width=600, height=400)
 windows.title("first program")
-
+windows.config(padx=100,pady=200)
 my_label = tkinter.Label(text="I am a label ", font=("arial", 30))
-my_label.pack()  # side=left right
-my_label["text"] = "Flower natherland"  # overwrite
-my_label.config(text="Germany")  # overwrite
+my_label.grid(column=1,row=4)  # side=left right
+
 
 
 # Button Class
@@ -17,11 +16,11 @@ def click_btn():
 
 
 button = tkinter.Button(text="click Me ", command=click_btn)
-button.pack()
+button.grid(column=3,row=4)
 
 #Entry Class
 entry=tkinter.Entry(width=20)
 print(entry.get())
-entry.pack()
+entry.grid(column=4,row=12)
 
 windows.mainloop()
