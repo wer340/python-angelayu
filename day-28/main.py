cf3e0222a0ec2e2ad6818b30147cf307b458130d
wer340 <hsee1372@gmail.com>
2022-07-30 17:15:18 +0430

    tick complete

--- day-28/main.py
+++ day-28/main.py
@@ -11,10 +11,11 @@ WORK_MIN = 0.1
 SHORT_BREAK_MIN = .15
 LONG_BREAK_MIN = 20
 reps = 0  # repetion
-
+timer=None
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset_timer():
+    tk.after_cancel(timer)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_countdown():
     # first reps declare next line  reps work
@@ -44,9 +45,15 @@ def countdown(count):
         time_sec = f"0{time_sec}"
     canv.itemconfig(timer_text, text=f"{time_min}:{time_sec}")
     if count > 0:
-        tk.after(1000, countdown, count - 1)
+       global timer
+       timer= tk.after(1000, countdown, count - 1)
     else:
         start_countdown()
+        mark=""
+        work_session=math.floor(reps/2)
+        for _ in range(work_session):
+            mark+="✔"
+        tick_l.config(text=mark)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -56,10 +63,12 @@ tk.config(padx=100, pady=50, bg=YELLOW)
 
 timer_l = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 35, "bold"),
                 fg=GREEN)
+
 timer_l.grid(row=1, column=2)
 canv = Canvas(width=200, height=224, bg=YELLOW)
 image = PhotoImage(file="./tomato.png")
 canv.create_image(102, 110, image=image)
+
 timer_text = canv.create_text(100, 113, text="00:00",
                               font=(FONT_NAME, 25, "bold"),
                               fill="white")
@@ -69,10 +78,10 @@ start_b = Button(text="start", bg="white", highlightthickness=0,
                  command=start_countdown)
 start_b.grid(row=3, column=1)
 
-reset_b = Button(text="reset", bg="white", highlightthickness=0)
+reset_b = Button(text="reset", bg="white", highlightthickness=0,command=reset_timer)
 reset_b.grid(row=3, column=3)
 
-tick_l = Label(text="✔")
+tick_l = Label(font=(FONT_NAME,25,"bold") ,bg=YELLOW,fg=GREEN)
 tick_l.grid(row=4, column=2)
 
 tk.mainloop()
