1e6fb99ea2a43ec1515d07daa87eb43dfb067f34
wer340 <hsee1372@gmail.com>
2022-08-02 09:52:26 +0430

    raise throw error

---
+++ day-30/raise_way.py
@@ -0,0 +1,10 @@
+
+
+height=float(input("inter your height meter ? : "))
+if height>3:
+    raise ValueError("your height shouldn't be over 3 meter")
+weight=float(input("inter your weight kg ? : "))
+bmi=weight/height**2
+bmi=round(bmi,2)
+print(f"your bmi is {bmi}")
+# raise TypeError("belaaa")
