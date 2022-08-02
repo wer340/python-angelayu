40c3b0180e3e3ae6e4218edecbb918000e729882
wer340 <hsee1372@gmail.com>
2022-08-02 09:35:15 +0430

    handle try except else finally

--- day-30/catch_exception.py
+++ day-30/catch_exception.py
@@ -1,13 +1,15 @@
 
 try:
-   file=open("daata.tx")
+   file=open("data.txt" )
+   a_dictionary = {"key": "value"}
+   value = a_dictionary["non_exist_key"]
+except FileNotFoundError:
+    file=open("data.txt",mode="w")
+    file.write("sample something")
+except KeyError as error_m:
+    print(f"key error{error_m} happen please make edit")
+else:
+    print(file.read())
 
-
-except:
-    print("file not exist")
-
-# else:
-#     file.read()
-#
-# finally:
-#     file.close()
+finally:
+    file.close()
