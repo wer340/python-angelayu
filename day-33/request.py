509bf1b2ee315cea5b5582d34692210cadf896a4
wer340 <hsee1372@gmail.com>
2022-08-28 08:53:37 +0430

    error handle by request module

--- day-33/request.py
+++ day-33/request.py
@@ -1,7 +1,4 @@
 import requests
 
 response=requests.get("http://api.open-notify.org/iss-now.json")
-if response.status_code==404:
-    raise Exception("this request is not exist")
-elif response.status_code==401:
-    raise Exception("You not Authorized for this request ")
+response.raise_for_status()
