13ba333069fa66fb3534aff150e5bc21e7cbaf46
wer340 <hsee1372@gmail.com>
2022-08-28 08:52:24 +0430

    raise exception for each state by manual

--- day-33/request.py
+++ day-33/request.py
@@ -1,5 +1,7 @@
 import requests
 
 response=requests.get("http://api.open-notify.org/iss-now.json")
-if response.status_code!=200:
-    raise Exception("Bad request to ISS")
+if response.status_code==404:
+    raise Exception("this request is not exist")
+elif response.status_code==401:
+    raise Exception("You not Authorized for this request ")
